8c2f025b16c299ec6723eaca51f966039e9dd2a4
l-bit-1 <lemanaog@gbox.adnu.edu.ph>
2026-09-15 20:01:19 +0800

    added icons for UI improvement

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,10 @@
 module ApplicationHelper
-
-  def sidebar_link(label, path, controller)
+  def sidebar_link(label, path, controller, icon)
     css = "nav-link text-white"
     css += " active" if controller_name == controller
 
-    link_to label, path, class: css
+    link_to path, class: css do
+      content_tag(:i, "", class: "bi #{icon} me-2") + label
+    end
   end
 end
